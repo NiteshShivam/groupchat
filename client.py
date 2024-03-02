17e71b76448ac449e269e7027b2a77f86fa59c12
Nitesh Yadav <42939377+NiteshShivam@users.noreply.github.com>
2024-03-02 15:06:19 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,102 +1,110 @@
 import socket
 import threading
-host = '127.0.0.1'#10.30.56.102
+import tkinter
+import tkinter.scrolledtext
+from tkinter import simpledialog
+
+
+host = '127.0.0.1'
 port = 9000
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind((host,port))
-server.listen()
-clients= []
-nicknames = []
+class Client:
+	def __init__(self,host,port):
+		self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		self.sock.connect((host,port))
+		msg = tkinter.Tk()
+		msg.withdraw()
+		self.nickname = simpledialog.askstring("nickname","Please choose a nickname",parent=msg)
+		self.gui_done=False
+		self.running = True
+		gui_thread = threading.Thread(target=self.gui_loop)
+		receive_thread = threading.Thread(target=self.receive)
+		gui_thread.start()
+		receive_thread.start()
+	def gui_loop(self):
+		self.win = tkinter.Tk()
+		self.win.configure(bg="lightgray")
 
-#broadcast
-def broadcast(message):
-	for client in clients:
-		client.send(message)
+		self.chat_label = tkinter.Label(self.win,text =self.nickname,bg="lightgray")
+		self.chat_label.config(font=("Arial",12))
+		self.chat_label.pack(padx=20,pady=5)
 
-def handle(client):
-	while True:
-		try:
-			message = client.recv(1024)
-			m = message
-			message = message.decode('utf-8')
-			result = message.split()
-			print('outside = ',result)
-			if 'calculate:' in message:
-				expression = message.split('calculate:')[1].strip()
-				try:
-					result = eval(expression)
-					client.send(f"Server: Result: {result}".encode('utf-8'))
-				except Exception as e:
-					client.send(f"Server: Error: {str(e)}".encode('utf-8'))
-			elif result[0]=='@':
-				print('bol bhai ',result)
-				client2 = result[2]
-				#client2 = b+'\''+client2+'\''
-				client2 = bytes(client2,'utf-8')
+		self.text_area = tkinter.scrolledtext.ScrolledText(self.win)
+		self.text_area.pack(padx=20,pady=5)
+		self.text_area.config(state='disabled')
 
-				#client2 = b'\'{}\''.format(client2)
-				print("mujhe bhejana ",client2)
-				print("nicknames = ",nicknames)
-				if client2 in nicknames:
-					print("inside ",result)
-					#index = clients.index(client2)
-					#indices_to_exclude = [1]
-					result_string = ' '.join(result[3:])
-					result_string = result[1]+result_string
-					#length_result = len(result)
-					#for l in range(length_result):
-					#	if l!=2:
-					#		result_string = result_string+ " "+ result[l]
-					#result_string = ' '.join([string for i, string in enumerate(string_list) if i not in indices_to_exclude])
-					clients[nicknames.index(client2)].send(result_string.encode('utf-8'))
+		self.msg_label = tkinter.Label(self.win,text ="Message:",bg="lightgray")
+		self.chat_label.config(font=("Arial",12))
+		self.chat_label.pack(padx=20,pady=5)
+		self.input_area = tkinter.Text(self.win,height=3)
+		self.input_area.pack(padx=20,pady=5)
+
+		self.send_button = tkinter.Button(self.win,text="send",command=self.write)
+		self.send_button.config(font=("Arial",12))
+		self.send_button.pack(padx=20,pady=5)
+		#my own 
+		self.calculate_button = tkinter.Button(self.win, text="Calculate", command=self.calculate)
+		self.calculate_button.config(font=("Arial", 12))
+		self.calculate_button.pack(padx=20, pady=5)
+
+		self.send_specific = tkinter.Button(self.win,text="specific user",command=self.specific)
+		self.send_specific.config(font=("Arial",12))
+		self.send_specific.pack(padx=20,pady=5)
+
+		self.all_client_button = tkinter.Button(self.win,text="know all user",command=self.allClient)
+		self.all_client_button.config(font=("Arial",12))
+		self.all_client_button.pack(padx=20,pady=5)
+
+		self.gui_done = True
+		self.win.protocol("WM_DELETE_WINDOW",self.stop)
+
+
+
+		
+
+
+		self.win.mainloop()
+	def allClient(self):
+		message = f"# {self.nickname}"
+		self.sock.send(message.encode('utf-8'))
+
+
+	def calculate(self):
+		expression = self.input_area.get('1.0', 'end').strip()
+		message = f"calculate: {expression}"
+		self.sock.send(message.encode('utf-8'))
+		self.input_area.delete('1.0', 'end')
+	def specific(self):
+		message = f"@ {self.nickname} {self.input_area.get('1.0','end')}"
+		self.sock.send(message.encode('utf-8'))
+		self.input_area.delete('1.0','end')
+	def write(self):
+		message = f"{self.nickname} : {self.input_area.get('1.0','end')}"
+		self.sock.send(message.encode('utf-8'))
+		self.input_area.delete('1.0','end')
+
+
+
+	def stop(self):
+		self.running = False
+		self.win.destroy()
+		self.sock.close()
+		exit(0)
+	def receive(self):
+		while self.running:
+			try:
+				message = self.sock.recv(1024).decode('utf-8')
+				if message =='NICK':
+					self.sock.send(self.nickname.encode('utf-8'))
 				else:
-					client.send("not able to send to the suggest user".encode('utf-8'))
-			else:
-				#message = client.recv(1024)
-				print(f"{nicknames[clients.index(client)]} says {m}")
-				broadcast(m)
-
-
-			#mine
-			'''message = client.recv(1024).decode('utf-8')
-            if message == 'NICK':
-                nickname = client.recv(1024).decode('utf-8')
-                nicknames.append(nickname)
-                clients.append(client)
-                broadcast(f"{nickname} has joined the chat!\n".encode('utf-8'))
-            else:
-                if 'calculate:' in message:
-                    # Extract the expression after 'calculate:'
-                    expression = message.split('calculate:')[1].strip()
-                    try:
-                        result = eval(expression)
-                        client.send(f"Server: Result: {result}".encode('utf-8'))
-                    except Exception as e:
-                        client.send(f"Server: Error: {str(e)}".encode('utf-8'))'''
-		except Exception as e:
-			print("exception as ",e)
-			index = clients.index(client)
-			clients.remove(client)
-			client.close()
-			nickname = nicknames[index]
-			broadcast(f"{nickname} has left the chat!\n".encode('utf-8'))
-			nicknames.remove(nickname)
-			break
-#receive
-def receive():
-	while True:
-		client,address= server.accept()
-		print(f"Connected with {str(address)} ")
-		client.send("NICK".encode('utf-8'))
-		nickname = client.recv(1024)
-		nicknames.append(nickname)
-		clients.append(client)
-		print(f"nickname of the clinet is {nickname}")
-		broadcast(f"{nickname} connected to the server\n".encode('utf-8'))
-		client.send("Connected to the server\n".encode('utf-8'))
-		thread = threading.Thread(target=handle,args=(client,))
-		thread.start()
-
-print("Server")
-receive()
-
+					if self.gui_done:
+						self.text_area.config(state='normal')
+						self.text_area.insert('end',message)
+						self.text_area.yview('end')
+						self.text_area.config(state='disabled')
+			except ConnectionAbortedError:
+				break
+			except:
+				print("Error")
+				self.sock.close()
+				break
+client = Client(host,port)
